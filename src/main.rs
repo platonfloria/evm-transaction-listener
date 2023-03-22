3f89787fb4bafd93ff171ebb00a914e025ac5469
Platon Floria <platon.floria@gmail.com>
2023-03-22 21:12:14 +0800

    feat: use block_with_txs

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,14 @@ use chrono::{DateTime, Utc};
 use tokio::sync::{mpsc::{self, UnboundedSender, UnboundedReceiver}, Mutex};
 use web3::{
     futures::StreamExt,
-    transports::{WebSocket, Batch},
-    types::{Address, Block, BlockHeader, BlockId, BlockNumber, H256, Transaction, TransactionId},
+    transports::WebSocket,
+    types::{Address, BlockHeader, BlockId, BlockNumber, Transaction},
     Web3
 };
 
 
 struct Controller {
     web3: web3::Web3<WebSocket>,
-    web3_batch: web3::Web3<Batch<WebSocket>>,
     watched_addresses: Mutex<HashSet<Address>>,
 }
 
@@ -26,10 +25,9 @@ fn iso8601(st: &std::time::SystemTime) -> String {
 
 
 impl Controller {
-    pub fn new(web3: web3::Web3<WebSocket>, web3_batch: web3::Web3<Batch<WebSocket>>) -> Result<Self> {
+    pub fn new(web3: web3::Web3<WebSocket>) -> Result<Self> {
         Ok(Controller {
             web3,
-            web3_batch,
             watched_addresses: Mutex::new(HashSet::new()),
         })
     }
@@ -55,27 +53,14 @@ impl Controller {
                 println!("{:?}", block_header);
 
                 let block_number = block_header.number.ok_or(eyre!("header does not contain block number"))?;
-                let block = self.web3.eth().block(BlockId::Number(BlockNumber::Number(block_number))).await?.ok_or(eyre!("unable to get the block"))?;
-                let mut transactions = self.get_block_transactions(block).await?;
+                let block = self.web3.eth().block_with_txs(BlockId::Number(BlockNumber::Number(block_number))).await?.ok_or(eyre!("unable to get the block"))?;
+                let mut transactions = block.transactions;
                 self.filter_relevant_transactions(&mut transactions).await;
                 for transaction in transactions {
                     self.process_transaction(timestamp, transaction).await?;
                 }
             }
         }
-    }
-
-    async fn get_block_transactions(&self, block: Block<H256>) -> Result<Vec<Transaction>> {
-        let mut transaction_requests = Vec::new();
-        for transaction_hash in block.transactions {
-            transaction_requests.push(self.web3_batch.eth().transaction(TransactionId::Hash(transaction_hash)));
-        }
-        self.web3_batch.transport().submit_batch().await?;
-        let mut transactions = Vec::new();
-        for transaction_request in transaction_requests {
-            transactions.push(transaction_request.await?.ok_or(eyre!("unable to query transaction"))?);
-        }
-        Ok(transactions)
     }
 
     async fn filter_relevant_transactions(&self, transactions: &mut Vec<Transaction>) {
@@ -105,11 +90,10 @@ impl Controller {
 async fn start() -> Result<()> {
     let websocket = WebSocket::new(&env::var("RPC_WS_URL").expect("RPC_WS_URL env variable not set")).await?;
     let web3 = Web3::new(websocket.clone());
-    let web3_batch = Web3::new(Batch::new(websocket));
 
     let (block_sender, block_receiver) = mpsc::unbounded_channel();
 
-    let controller = Arc::new(Controller::new(web3, web3_batch)?);
+    let controller = Arc::new(Controller::new(web3)?);
 
     match tokio::join!(
         controller.listen_to_new_blocks(block_sender),
